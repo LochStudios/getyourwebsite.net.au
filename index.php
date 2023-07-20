10bb4f43c0bb6410d4b05bad48eca9c1d68c1fb8
gfaUnDead <contact@gfaundead.stream>
2023-07-20 22:37:13 +1000

    Create index.php

---
+++ index.php
@@ -0,0 +1,132 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>GetYourWebsite | Welcome</title>
+    <link rel="icon" href="https://lscdn.site/static/logos/lochstudios/LSLarge.png" type="image/png" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/foundation.min.css">
+  </head>
+  <body>
+
+    <div class="off-canvas-wrapper">
+      <div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
+        <div class="off-canvas-content" data-off-canvas-content>
+          <div class="title-bar hide-for-large">
+            <div class="title-bar-left">
+              <img src="https://lscdn.site/static/logos/lochstudios/LSLarge.png" width="25px" height="25px"> <span class="title-bar-title">GetYourWebsite</span>
+            </div>
+          </div>
+          <div class="callout primary">
+            <div class="row column">
+              <h1><!--<img src="https://lscdn.site/static/logos/lochstudios/LSLarge.png" width="50px" height="50px">-->GetYourWebsite</h1>
+              <p class="lead">Here is the full list of website that we have compleated that is still online to this day.</p>
+            </div>
+          </div>
+          <div class="row small-up-2 medium-up-3 large-up-4">
+            <div class="column">
+              <a href="https://lochstudios.info/" target="_blank"><img class="thumbnail" src="assets/images/sites/lochsutdios.jpg" width="500px" height="500px"></a>
+              <h5>LochStudios</h5>
+            </div>
+            <div class="column">
+              <a href="https://fourflavors.net/" target="_blank"><img class="thumbnail" src="assets/images/sites/fourflavors.jpg" width="500px" height="500px"></a>
+              <h5>FourFlavors</h5>
+            </div>
+            <div class="column">
+              <a href="https://gamingforaustralia.com.au/" target="_blank"><img class="thumbnail" src="assets/images/sites/gamingforaustralia.jpg" width="500px" height="500px"></a>
+              <h5>GamingForAustralia</h5>
+            </div>
+            <div class="column">
+              <a href="https://musicbots.app/" target="_blank"><img class="thumbnail" src="assets/images/sites/musicbots.jpg" width="500px" height="500px"></a>
+              <h5>MusicBots</h5>
+            </div>
+            <hr>
+            <div class="column">
+              <a href="https://gfaundead.stream/" target="_blank"><img class="thumbnail" src="assets/images/sites/gfaundead.jpg" width="500px" height="500px"></a>
+              <h5>gfaUnDead</h5>
+            </div>
+            <div class="column">
+              <a href="https://lscomputerrepair.com/" target="_blank"><img class="thumbnail" src="assets/images/sites/lscomputerrepair.jpg" width="500px" height="500px"></a>
+              <h5>LS Computer Repair<br>(GraftonIT)</h5>
+            </div>
+            <div class="column">
+              <a href="https://sentral.network/" target="_blank"><img class="thumbnail" src="assets/images/sites/SentralNetwork.jpg" width="500px" height="500px"></a>
+              <h5>SentralNetwork</h5>
+            </div>
+            <div class="column">
+              <a href="https://thelaunchcraft.net.au/" target="_blank"><img class="thumbnail" src="assets/images/sites/thelaunchcraft.jpg" width="500px" height="500px"></a>
+              <h5>TheLaunchCraft</h5>
+            </div>
+            <hr>
+            <div class="column">
+              <a href="https://marthascreations.com/" target="_blank"><img class="thumbnail" src="assets/images/sites/marthascreations.jpg" width="500px" height="500px"></a>
+              <h5>Martha's Creations</h5>
+            </div>
+            <div class="column">
+              <a href="https://bibleofbotany.com/" target="_blank"><img class="thumbnail" src="assets/images/sites/bibleofbotany.jpg" width="500px" height="500px"></a>
+              <h5>The Bible Of Botany</h5>
+            </div>
+            <div class="column">
+              <a href="https://mrbadly.com/" target="_blank"><img class="thumbnail" src="assets/images/sites/mrbadly.jpg" width="500px" height="500px"></a>
+              <h5>Mr. Badly Productions</h5>
+            </div>
+            <div class="column">
+              <a href="https://nourishmyheart.net/" target="_blank"><img class="thumbnail" src="assets/images/sites/nourishmyheart.jpg" width="500px" height="500px"></a>
+              <h5>Nourish My Heart</h5>
+            </div>
+            <hr>
+            <div class="column">
+              <a href="https://oztiks.com/" target="_blank"><img class="thumbnail" src="assets/images/sites/oztiks.jpg" width="500px" height="500px"></a>
+              <h5>OzTiKs546</h5>
+            </div>
+            <div class="column">
+              <a href="https://counsellingforthevalley.com.au/" target="_blank"><img class="thumbnail" src="assets/images/sites/counsellingforthevalley.jpg" width="500px" height="500px"></a>
+              <h5>Counselling For The Valley</h5>
+            </div>
+            <div class="column">
+              <a href="https://producerjayce.com/" target="_blank"><img class="thumbnail" src="assets/images/sites/producerjayce.jpg" width="500px" height="500px"></a>
+              <h5>Producer Jayce</h5>
+            </div>
+          </div>
+          <hr>
+          <div class="row">
+            <div class="medium-6 columns">
+              <h3>Contact Us:</h3>
+              <ul class="menu">
+                <li><a href="https://www.facebook.com/lochstudios/" target="_blank"class="fa fa-facebook"></a></li>
+                <li><a href="https://twitter.com/LochStudios" target="_blank"class="fa fa-twitter"></a></li>
+                <li><a href="https://www.instagram.com/lochstudiosau/" target="_blank"class="fa fa-instagram"></a></li>
+                <li><a href="https://github.com/LochStudios" target="_blank"class="fa fa-github"></a></li>
+                <li><a href="mailto://makeme@getyourwebsite.net.au" target="_blank"class="fa fa-envelope"></a></li>
+                <li><a href="tel://+61256323092" target="_blank"class="fa fa-phone"></a></li>
+                <br>
+                <p><strong>Mail Us</strong>: PO Box 219, South Grafton, New South Wales, 2460, Australia
+                <br><strong>Phone Us</strong>: <a href="tel:+61256323092">(02) 5632-3092</a> or
+                <br><strong>International</strong>: <a href="tel:+13158796488">+1 (315) 879-6488</a>
+                <br><strong>In the phone menu; option 4 or extension 5000</strong>
+                <br><strong>Direct Line</strong>: <a href="tel:+61280067521">(02) 800-67521</a>
+                <br>Send us a <strong>FAX</strong> at this number: (02) 5632-3095</p>
+              </ul>
+            </div>
+            <div class="medium-6 columns">
+              <div class="copyright">
+                <p>Copyright &copy; 2022 All rights reserved.<br><strong>LochStudios</strong> and any of <strong>LochStudios</strong> subsidiaries are registered under: ABN: <a href="https://abr.business.gov.au/ABN/View/20447022747" target="_blank">20 447 022 747</a><br>
+                <br />Powered By: <a href="https://getyourwebsite.net.au" target="_blank">GetYourWebsite</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script src="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
+
+
+
